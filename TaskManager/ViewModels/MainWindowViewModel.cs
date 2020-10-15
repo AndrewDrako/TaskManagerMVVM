a91d24896ca0e6f749b01c0722a6e5a52029c522
3954014@gmail.com <3954014@gmail.com>
2020-10-15 18:21:09 +0300

    Создание ViewModel для MainWindow Класс MainWindowViewModel

---
+++ TaskManager/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.ViewModels.Base;
+
+namespace TaskManager.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок окна
+
+        private string _Title = "Task Manager";
+
+        public string Title
+        {
+            get => _Title;
+
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+
+        #region Название кнопки номер 1
+
+        private string _ButtonContent1 = "Home";
+
+        public string ButtonContent1
+        {
+            get => _ButtonContent1;
+
+            set => Set(ref _ButtonContent1, value);
+        }
+
+        #endregion
+
+        #region Название кнопки номер 2
+
+        private string _ButtonContent2 = "Dashboard";
+
+        public string ButtonContent2
+        {
+            get => _ButtonContent2;
+
+            set => Set(ref _ButtonContent2, value);
+        }
+
+        #endregion
+    }
+}
