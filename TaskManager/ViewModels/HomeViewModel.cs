4c125c2c7fe740e36351f0e1808fe9d59380a489
3954014@gmail.com <3954014@gmail.com>
2020-10-16 21:12:16 +0300

    Готово - добро пожаловать и эл. адрес

--- TaskManager/ViewModels/HomeViewModel.cs
+++ TaskManager/ViewModels/HomeViewModel.cs
@@ -9,6 +9,23 @@ namespace TaskManager.ViewModels
 {
     internal class HomeViewModel : ViewModel
     {
+        #region Добро пожаловать .com
 
+        private string _Welcome = "Welcome";
+        private string _Email = "3954014@gmail.com";
+
+        public string Welcome
+        {
+            get => _Welcome;
+            set => Set(ref _Welcome, value);
+        }
+
+        public string Email
+        {
+            get => _Email;
+            set => Set(ref _Email, value);
+        }
+
+        #endregion
     }
 }
