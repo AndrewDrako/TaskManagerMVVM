ac999fea714721bca97356af169137bcf5198d82
3954014@gmail.com <3954014@gmail.com>
2020-10-18 20:58:03 +0300

    Создание View Model для CreateProjectWindow

---
+++ TaskManager/ViewModels/CreateProjectWindowViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using TaskManager.ViewModels.Base;
+
+namespace TaskManager.ViewModels
+{
+    class CreateProjectWindowViewModel : ViewModel
+    {
+        private string _ProjectName;
+
+        public string ProjectName
+        {
+            get => _ProjectName;
+            set => Set(ref _ProjectName, value);
+        }
+
+        private string _TeamName;
+
+        #region Название кнопки OK
+
+        private string _ButtonOKContent = "OK";
+
+        public string ButtonOKContent
+        {
+            get => _ButtonOKContent;
+            set => Set(ref _ButtonOKContent, value);
+        }
+
+
+        #endregion
+
+        #region Enter project name
+
+        private string _EnterProjectNameLabel = "Enter project name";
+
+        public string EnterProjectNameLabel
+        {
+            get => _EnterProjectNameLabel;
+            set => Set(ref _EnterProjectNameLabel, value);
+        }
+
+        #endregion
+
+        #region Enter team name
+
+        private string _EnterTeamNameLabel = "Enter owner name or team name";
+
+        public string EnterTeamNameLabel
+        {
+            get => _EnterTeamNameLabel;
+            set => Set(ref _EnterTeamNameLabel, value);
+        }
+
+        #endregion
+
+        #region Button OK click and save info
+
+        public ICommand ButtonClick { get; }
+
+        private bool CanButtonClickExecute(object p) => true;
+
+        private void OnButtonClickExecuted(object p)
+        {
+            
+        }
+
+        #endregion
+    }
+}
