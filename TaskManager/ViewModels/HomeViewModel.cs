f07cad0dbf3aaf02e9fe56c94a1a788f7be31cba
3954014@gmail.com <3954014@gmail.com>
2020-10-22 19:33:45 +0300

    работаю с обновлением названия проекта на кнопке

--- TaskManager/ViewModels/HomeViewModel.cs
+++ TaskManager/ViewModels/HomeViewModel.cs
@@ -68,17 +68,9 @@ namespace TaskManager.ViewModels
 
         #endregion
 
-        #region Имя проекта
+        
 
-        private string _PName;
-
-        public string PName
-        {
-            get => CreateProjectModel.ProjectName[0];
-            set => Set(ref CreateProjectModel.ProjectName[0], value);
-        }
-
-        #endregion
+        
 
         /// <summary>
         /// Commands
@@ -98,7 +90,7 @@ namespace TaskManager.ViewModels
             {
                 this.ChangeControlVisibility = Visibility.Visible;
             }
-            
+
 
         }
 
@@ -116,7 +108,111 @@ namespace TaskManager.ViewModels
             set => Set(ref _Visibility, value);
             
         }
-        
+
+
+        #endregion
+
+        /// <summary>
+        /// Бывшая CreateProjectWindowViewModel.cs
+        /// </summary>
+
+        #region Имя проекта и имя команды Label
+
+        private string _ProjectName;
+
+        public string ProjectName
+        {
+            get => _ProjectName;
+            set => Set(ref _ProjectName, value);
+        }
+
+        private string _TeamName;
+
+        public string TeamName
+        {
+            get => _TeamName;
+            set => Set(ref _TeamName, value);
+        }
+
+        #endregion
+
+        #region Название кнопки OK
+
+        private string _ButtonOKContent = "OK";
+
+        public string ButtonOKContent
+        {
+            get => _ButtonOKContent;
+            set => Set(ref _ButtonOKContent, value);
+        }
+
+
+        #endregion
+
+        #region Enter project name
+
+        private string _EnterProjectNameLabel = "Enter project name";
+
+        public string EnterProjectNameLabel
+        {
+            get => _EnterProjectNameLabel;
+            set => Set(ref _EnterProjectNameLabel, value);
+        }
+
+        #endregion
+
+        #region Enter team name
+
+        private string _EnterTeamNameLabel = "Enter owner name or team name";
+
+        public string EnterTeamNameLabel
+        {
+            get => _EnterTeamNameLabel;
+            set => Set(ref _EnterTeamNameLabel, value);
+        }
+
+        #endregion
+
+        #region Button OK click and save info
+
+        public ICommand ButtonClick { get; }
+
+        private bool CanButtonClickExecute(object p) => true;
+
+        private void OnButtonClickExecuted(object p)
+        {
+            #region Закрытие
+
+            Window window = p as Window;
+
+            if (window is null)
+            {
+                window = Application.Current.Windows.Cast<Window>().FirstOrDefault(w => w.IsFocused);
+            }
+
+            if (window is null)
+            {
+                window = Application.Current.Windows.Cast<Window>().FirstOrDefault(w => w.IsActive);
+            }
+
+            window?.Close();
+
+            #endregion
+
+            #region ПЕредаем данные
+
+            CreateProjectModel.SetProjectName(_ProjectName);
+            CreateProjectModel.SetTeamName(_TeamName);
+            CreateProjectModel.PrintToTxt();
+
+            MainWindowModel.IsTasksNotEmpty = true;
+
+
+            
+
+
+            #endregion
+        }
 
         #endregion
 
@@ -126,13 +222,14 @@ namespace TaskManager.ViewModels
         {
             #region Окна создания
             //_CreateProjectWindow = new Views.Windows.CreateProjectWindowxaml();
-            
+
 
             #endregion
 
             #region Комманды
 
             CreateProjectClick = new LambdaCommand(OnCreateProjectClickExecuted, CanCreateProjectClickExecute);
+            ButtonClick = new LambdaCommand(OnButtonClickExecuted, CanButtonClickExecute);
 
             #endregion
         }
