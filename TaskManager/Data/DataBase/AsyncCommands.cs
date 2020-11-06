6d3c5e7118f993b0b1908ad55744e8ae0d92c8c2
3954014@gmail.com <3954014@gmail.com>
2020-11-06 09:47:49 +0300

    Создание асинхронных методов работы с бд

---
+++ TaskManager/Data/DataBase/AsyncCommands.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Data.DataBase.Base;
+using TaskManager.Data.DataBase.Tables;
+
+namespace TaskManager.Data.DataBase
+{
+    public class AsyncCommands
+    {
+        /// <summary>
+        /// Подключение к БД
+        /// </summary>
+        /// <param name="db"></param>
+        /// <returns></returns>
+        public async Task ConnectToDB(MyDbContext db)
+        {
+            await Task.Run(() => db = new MyDbContext());
+            LoadDataFromDB(db);
+        }
+
+        /// <summary>
+        /// Загрузка данных из БД
+        /// </summary>
+        /// <param name="db"></param>
+        /// <returns></returns>
+        public async Task LoadDataFromDB(MyDbContext db)
+        {
+            await Task.Run(() => db.Users.Load());
+        }
+
+        /// <summary>
+        /// Добавление в БД
+        /// </summary>
+        /// <param name="db"></param>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public async Task AddToDB(MyDbContext db, User user)
+        {
+            await Task.Run(() => db.Users.Add(user));
+        }
+
+        /// <summary>
+        /// Удаление из БД
+        /// </summary>
+        /// <param name="db"></param>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public async Task RemoveFromDB(MyDbContext db, User user)
+        {
+            await Task.Run(() => db.Users.Remove(user));
+        }
+
+        //public async Task GetDataFromDB(MyDbContext db, User user, )
+
+
+    }
+}
