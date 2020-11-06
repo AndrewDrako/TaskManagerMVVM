94b96853aa660a4e1684550d0393ddae573af60c
3954014@gmail.com <3954014@gmail.com>
2020-11-06 09:17:19 +0300

    Шаг2: создание класса MyDbContext-конструктора

---
+++ TaskManager/Data/DataBase/Base/MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Data.DataBase.Base
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("DefaultConnection")
+        {
+            
+        }
+
+    }
+}
